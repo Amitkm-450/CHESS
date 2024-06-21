0a6c8015b4b03ba59830626fa5540ba0979e0ccc
amit kumar <mohapatraamitkumar450@gmail.com>
2024-06-22 02:19:50 +0530

    Game manager and Game class in progress

---
+++ backend1/dist/GameManager.js
@@ -0,0 +1,37 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GameManager = void 0;
+const Game_1 = require("./Game");
+const Messages_1 = require("./Messages");
+class GameManager {
+    constructor() {
+        this.games = [];
+        this.users = [];
+        this.pendingUser = null;
+    }
+    addUser(socket) {
+        this.users.push(socket);
+        this.addHandler(socket);
+    }
+    removeUser(socket) {
+        this.users = this.users.filter((user) => user !== socket);
+        //stop the game as the user left
+    }
+    addHandler(socket) {
+        socket.on('message', (data) => {
+            const message = JSON.parse(data.toString());
+            if (message.type === Messages_1.INIT_GAME) {
+                if (this.pendingUser) {
+                    // connect to the pending user and start the game
+                    const game = new Game_1.Game(this.pendingUser, socket);
+                    this.pendingUser = null;
+                    this.games.push(game);
+                }
+                else {
+                    this.pendingUser = socket;
+                }
+            }
+        });
+    }
+}
+exports.GameManager = GameManager;
